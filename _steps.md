9f9c949598436ab02ed647cc9999319e3c4d44c9
redhat-debug <https://github.com/redhat-debug/ishq.git>
2022-10-13 20:34:41 +0600

    sign o=in - out implemented

--- _steps.md
+++ _steps.md
@@ -1,5 +1,5 @@
 # steps
-
+initial setup ///
 * visit firebase.google.com (console)
 * create a new firebase project
 * go to docs/firebase : Authentication > web > getting started
@@ -7,6 +7,9 @@
 * install firebase fir your project : npm install firebase
 * dangerous step: get firebase init codes and putting publicly
 * export firebase init codes (export default)
+// ------------
+ setup provider
+// -----------
 * create auth using getAuth from firebase by using app from fire base init file
 * create goole auth provider
 * go to firebase > build > authentication > sign in method 
@@ -14,4 +17,8 @@
 * create a button and method for google sign in
 * inside the event handler, call signInWithPopUp with auth, provider
 * after signInWithPopUp .then result, error
+
+// -----------
+ Display Data
+// -----------
 * 
--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
-import { getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithPopup } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
 import app from './firebase/firebase.init';
+import { useState } from 'react';
 
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({})
 
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
       .then(result => {
         const user = result.user;
+        setUser(user)
         console.log(user);
       })
       .catch(error => {
@@ -19,10 +22,37 @@ function App() {
       })
   }
 
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch(() => {
+        setUser({});
+      })
+  }
+
 
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google Sign in</button>
+
+      {(!user.email) ?
+        <div>
+          <br />
+          <button onClick={handleGoogleSignIn}>Google Sign in</button>
+        </div>
+        :
+        <div>
+          <br /> <br />
+          <button onClick={handleGoogleSignOut}>Sign Out</button>
+        </div>
+      }
+
+      {user.email && <div>
+        <p>User Name : {user.displayName}</p>
+        <p>Email : {user.email}</p>
+        <img src={user.photoURL} alt={user.displayName} />
+      </div>}
     </div>
   );
 }
